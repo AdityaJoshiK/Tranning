d430ab9f2e7c5a947155513b07a307c348774a3b
AdityaJoshiK <adityajoshi2107@gmail.com>
2023-10-23 16:40:32 +0530

    function

---
+++ Javascript/Function/Functionwithparam.js
@@ -0,0 +1,38 @@
+function greeting(msg = "GM") { //GM is default if user can't pass anything
+    console.log(msg);
+}
+
+greeting();
+
+//if function has multiple params and you don't know
+function name(...params) {
+    console.log(params);
+}
+
+name(1,2,3)
+
+function a(a,b,...params) { //in below 1,2 store in a,b and rest of(3,4,5) store in parmas
+    console.log(a,b,params);
+}
+
+a(1,2,3,4,5)
+
+//you can also pass object and array in paramters
+function obj(object) {
+    console.log(`Your name is ${object.name}`);
+}
+
+const abc = {
+    name: 'John',
+};
+
+obj(abc);
+
+//using array
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue([200, 400, 500, 1000]));
