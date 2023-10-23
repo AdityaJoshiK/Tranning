37c176ee39a87958d00700c72ee4705132ec2271
AdityaJoshiK <adityajoshi2107@gmail.com>
2023-10-23 19:01:10 +0530

    Guessthenumber

--- Javascript/practice/Guessthenumber.js
+++ Javascript/practice/Guessthenumber.js
@@ -3,14 +3,14 @@
 
         const n = Math.floor(Math.random()*100);
 
-        console.log(n);
-        document.write(n);
+        // console.log(n);
+        // document.write(n);
         let score =0;
         let a = prompt("Enter Number:");
         
         while (a!=n) {
             a=Number(a);
-            
+                
             if (a<n) {
                 console.log("Choose some large number")
             }
@@ -19,7 +19,7 @@
                 
             }
             score++;
-            let a = prompt("Enter Number:");
+            a = prompt("Enter Number:");
         }
         
         console.log(`your score is ${score}`)
