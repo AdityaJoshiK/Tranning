9cf51f1a817b69be2b02155d73cd84341f35c146
AdityaJoshiK <adityajoshi2107@gmail.com>
2023-10-24 15:18:41 +0530

    dateandtime

--- Javascript/Basics/DateandTime.js
+++ Javascript/Basics/DateandTime.js
@@ -1,16 +1,17 @@
 const date = new Date();
-console.log(date)
-console.log(date.toLocaleString())
-console.log(date.toDateString())
-console.log(date.toString())
+console.log(date);
+console.log(date.toLocaleString());
+console.log(date.toDateString());
+console.log(date.toString());
 
 // const newDate = new Date(2023,0,23);
 // const newDate = new Date(2023,0,23,5,3);
-const newDate =  new Date("01-14-2023")
+const newDate =  new Date("01-14-2023");
 
 console.log(newDate.toLocaleDateString());
 
 let daten = new Date();
+
 console.log(date.getDate())
 console.log(date.getMonth()+1)
 console.log(date.getDay())
