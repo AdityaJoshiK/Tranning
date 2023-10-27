5b6830ad82ed950395f33edbd0b90897526cdb52
AdityaJoshiK <adityajoshi2107@gmail.com>
2023-10-27 15:21:03 +0530

    Number guessing

---
+++ Javascript/DOM/Projects/GuessTheNumber/script.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('form');
+let guesses = 10;
+
+let min=1,max=100;
+
+let guess = Math.floor(Math.random()*(max-min))+1;
+document.write(guess);
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    const inp = parseInt(document.querySelector("#guessField").value);
+
+    if(inp === guess){
+        alert("Congratulations! You've got it right!");
+        location.reload();
+    }
+
+
+    const prevNode = document.createTextNode(inp+" ");
+    let prevGuesses = document.querySelector(".guesses");
+    prevGuesses.appendChild(prevNode);
+
+    guesses--;
+    document.querySelector(".lastResult").innerHTML=guesses;
+    if (guesses==0) {
+        alert("Try Next Time!");
+        
+        location.reload();
+    }
+})
