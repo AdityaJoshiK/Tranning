a5a11ea8dbdb56d02112f14e1890b43ca2658a76
AdityaJoshiK <adityajoshi2107@gmail.com>
2023-10-30 12:43:15 +0530

    data fetched in registation

--- Javascript/Task/Form Validation/script.js
+++ Javascript/Task/Form Validation/script.js
@@ -28,3 +28,36 @@ function login() {
     }
     return true;
 }
+
+function register() {
+    const regform = document.querySelector(".regform");
+
+    const fname = document.regform.fname.value;
+    const lname = document.regform.lname.value;
+    const pwd = document.regform.pwd.value;
+    const cpwd = document.regform.cpwd.value;
+    const dob = document.regform.dob.value;
+    const phone = document.regform.phone.value;
+    const email = document.regform.email.value;
+    const cemail = document.regform.cemail.value;
+    const address = document.regform.address.value;
+    const city = document.regform.city.value;
+    const state = document.regform.state.value;
+    const code = document.regform.code.value;
+    const country = document.regform.country.value;
+    console.log(fname);
+    console.log(lname);
+    console.log(pwd);
+    console.log(cpwd);
+    console.log(dob);
+    console.log(phone);
+    console.log(email);
+    console.log(cemail);
+    console.log(address);
+    console.log(city);
+    console.log(state);
+    console.log(code);
+    console.log(country);
+
+    return false;
+}
