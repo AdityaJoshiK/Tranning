374cbbdcb05185b438436648dfd1b3e19dd37f2e
AdityaJoshiK <adityajoshi2107@gmail.com>
2023-11-01 16:17:12 +0530

    data updated

--- Javascript/Task/Cascade Checkbox/script.js
+++ Javascript/Task/Cascade Checkbox/script.js
@@ -1,72 +1,151 @@
 const data = {
     "countries": [
-        {
-            "name": "United States",
-            "states": [
-                {
-                    "name": "New York",
-                    "cities": ["New York City", "Buffalo", "Albany"]
-                },
-                {
-                    "name": "California",
-                    "cities": ["Los Angeles", "San Francisco", "San Diego"]
-                }
-            ]
-        },
-        {
-            "name": "India",
-            "states": [
-                {
-                    "name": "Maharashtra",
-                    "cities": ["Mumbai", "Pune", "Nagpur"]
-                },
-                {
-                    "name": "Tamil Nadu",
-                    "cities": ["Chennai", "Coimbatore", "Madurai"]
-                }
-            ]
-        },
-        {
-            "name": "United Kingdom",
-            "states": [
-                {
-                    "name": "England",
-                    "cities": ["London", "Birmingham", "Manchester"]
-                },
-                {
-                    "name": "Scotland",
-                    "cities": ["Glasgow", "Edinburgh", "Aberdeen"]
-                }
-            ]
-        },
-        {
-            "name": "China",
-            "states": [
-                {
-                    "name": "Beijing",
-                    "cities": ["Beijing City", "Tongzhou", "Chaoyang"]
-                },
-                {
-                    "name": "Shanghai",
-                    "cities": ["Shanghai City", "Pudong", "Minhang"]
-                }
-            ]
-        },
-        {
-            "name": "Brazil",
-            "states": [
-                {
-                    "name": "São Paulo",
-                    "cities": ["São Paulo", "Campinas", "Santo André"]
-                },
-                {
-                    "name": "Rio de Janeiro",
-                    "cities": ["Rio", "Niterói", "Nova Iguaçu"]
-                }
-            ]
-        }
+      {
+        "name": "United States",
+        "states": [
+          {
+            "name": "New York",
+            "cities": ["New York City", "Buffalo", "Albany"]
+          },
+          {
+            "name": "California",
+            "cities": ["Los Angeles", "San Francisco", "San Diego"]
+          }
+        ]
+      },
+      {
+        "name": "India",
+        "states": [
+          {
+            "name": "Maharashtra",
+            "cities": ["Mumbai", "Pune", "Nagpur"]
+          },
+          {
+            "name": "Tamil Nadu",
+            "cities": ["Chennai", "Coimbatore", "Madurai"]
+          }
+        ]
+      },
+      {
+        "name": "United Kingdom",
+        "states": [
+          {
+            "name": "England",
+            "cities": ["London", "Birmingham", "Manchester"]
+          },
+          {
+            "name": "Scotland",
+            "cities": ["Glasgow", "Edinburgh", "Aberdeen"]
+          }
+        ]
+      },
+      {
+        "name": "China",
+        "states": [
+          {
+            "name": "Beijing",
+            "cities": ["Beijing City", "Tongzhou", "Chaoyang"]
+          },
+          {
+            "name": "Shanghai",
+            "cities": ["Shanghai City", "Pudong", "Minhang"]
+          }
+        ]
+      },
+      {
+        "name": "Brazil",
+        "states": [
+          {
+            "name": "São Paulo",
+            "cities": ["São Paulo", "Campinas", "Santo André"]
+          },
+          {
+            "name": "Rio de Janeiro",
+            "cities": ["Rio", "Niterói", "Nova Iguaçu"]
+          }
+        ]
+      },
+      {
+        "name": "Canada",
+        "states": [
+          {
+            "name": "Ontario",
+            "cities": ["Toronto", "Ottawa", "Hamilton"]
+          },
+          {
+            "name": "Quebec",
+            "cities": ["Montreal", "Quebec City", "Laval"]
+          }
+        ]
+      },
+      {
+        "name": "Australia",
+        "states": [
+          {
+            "name": "New South Wales",
+            "cities": ["Sydney", "Newcastle", "Wollongong"]
+          },
+          {
+            "name": "Victoria",
+            "cities": ["Melbourne", "Geelong", "Ballarat"]
+          }
+        ]
+      },
+      {
+        "name": "Germany",
+        "states": [
+          {
+            "name": "Bavaria",
+            "cities": ["Munich", "Nuremberg", "Augsburg"]
+          },
+          {
+            "name": "North Rhine",
+            "cities": ["Cologne", "Düsseldorf", "Dortmund"]
+          }
+        ]
+      },
+      {
+        "name": "Japan",
+        "states": [
+          {
+            "name": "Tokyo",
+            "cities": ["Tokyo", "Yokohama", "Chiba"]
+          },
+          {
+            "name": "Osaka",
+            "cities": ["Osaka", "Sakai", "Kobe"]
+          }
+        ]
+      },
+      {
+        "name": "South Africa",
+        "states": [
+          {
+            "name": "Gauteng",
+            "cities": ["Johannesburg", "Pretoria", "Soweto"]
+          },
+          {
+            "name": "Western Cape",
+            "cities": ["Cape Town", "Stellenbosch", "George"]
+          }
+        ]
+      },
+      {
+        "name": "Mexico",
+        "states": [
+          {
+            "name": "Mexico City",
+            "cities": ["Mexico City", "Ecatepec", "Nezahualcóyotl"]
+          },
+          {
+            "name": "Jalisco",
+            "cities": ["Guadalajara", "Zapopan", "Tlaquepaque"]
+          }
+        ]
+      }
     ]
-}
+  }
+  
 
 const countryCheckbox = document.getElementById("country");
 const stateCheckbox = document.getElementById("state");
@@ -126,6 +205,8 @@ countryCheckbox.addEventListener('change', (e) => {
                     div.appendChild(input);
                     div.appendChild(label);
                 }
+                
+                console.log("before",selectedStates)
                 if (!checked) {
                     if (i != country.states.length) {
                         document.querySelector(`#${state.name.replace(/\s/g, '')}`).parentNode.remove()
@@ -135,15 +216,13 @@ countryCheckbox.addEventListener('change', (e) => {
                         checked = true;
                     }
                     console.log(state,selectedStates.get(state.name))
-                    console.log(selectedStates.get(state.name.replace(/\s/g, '')))
-                    console.log(selectedStates)
                     if (selectedStates.has(state.name)) {
-                        // alert("true")
-                            state.cities.forEach((city)=>{
-                                document.querySelector(`#${city.replace(/\s/g, '')}`).parentNode.remove()
-                                selectedStates.delete(state.name)
-                            })
+                        state.cities.forEach((city)=>{
+                            document.querySelector(`#${city.replace(/\s/g, '')}`).parentNode.remove()
+                            selectedStates.delete(state.name)
+                        })
                     }
+                    console.log("after",selectedStates)
                 }
                 
             })
