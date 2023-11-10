36b231064e06834b0d79bf22b3d719eb35a89142
Aditya Joshi <90098094+AdityaJoshiK@users.noreply.github.com>
2023-11-10 17:07:03 +0530

    Add files via upload

---
+++ ASP .Net MVC/TranningMVCApp/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TranningMVCApp.Models
+{
+    [Table("CUSTOMERS")]
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string CName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ ASP .Net MVC/TranningMVCApp/Models/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TranningMVCApp.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set;}
+        public DbSet<Department> Departments { get; set; }
+
+        public System.Data.Entity.DbSet<TranningMVCApp.Models.Customer> Customers { get; set; }
+    }
+}
