ac31402c8e6de4332402ec6116b9987e169c02c8
AdityaJoshiK <adityajoshi2107@gmail.com>
2023-11-11 19:53:13 +0530

    Buisness layer adeed

---
+++ ASP .Net MVC/BusinessLayer/EmployeeBuisnessLayer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace BusinessLayer
+{
+    public class EmployeeBuisnessLayer
+    {
+        public IEnumerable<Employee> Employees
+        {
+            get
+            {
+                string connectionString =
+                    ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+                List<Employee> employees = new List<Employee>();
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spGetAllEmployees", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        Employee employee = new Employee();
+                        employee.EmployeeId = Convert.ToInt32(rdr["EmployeeId"]);
+                        employee.Name = rdr["Name"].ToString();
+                        employee.Gender = rdr["Gender"].ToString();
+                        employee.City = rdr["City"].ToString();
+                        employee.DateOfBirth = Convert.ToDateTime(rdr["DateOfBirth"]);
+
+                        employees.Add(employee);
+                    }
+                }
+
+                return employees;
+            }
+        }
+    }
+}
