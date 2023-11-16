1b719888dd0c412c273534c28b12dfa72f572d6f
AdityaJoshiK <adityajoshi2107@gmail.com>
2023-11-16 12:35:55 +0530

    Insert Done

--- ASP .Net MVC/TranningMVCApp/Controllers/EmployeeBLayerController.cs
+++ ASP .Net MVC/TranningMVCApp/Controllers/EmployeeBLayerController.cs
@@ -21,8 +21,28 @@ namespace TranningMVCApp.Controllers
         [HttpGet]
         public ActionResult Create()
         {
-
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection formCollection)
+        {
+            Employee employee = new Employee();
+            employee.Name= formCollection["Name"];
+            employee.City = formCollection["City"];
+            employee.Gender = formCollection["Gender"];
+            employee.DateOfBirth = Convert.ToDateTime(formCollection["DateOfBirth"]);
+
+            EmployeeBuisnessLayer employeeblayer = new BusinessLayer.EmployeeBuisnessLayer();
+            employeeblayer.addEmployee(employee);
+
+            //foreach(string key in formCollection.Keys)
+            //{
+            //    Response.Write("Key:" + key);
+            //    Response.Write(formCollection[key]);
+            //    Response.Write("</br>");
+            //}
+            return RedirectToAction("Index");
         }
     }
 }
