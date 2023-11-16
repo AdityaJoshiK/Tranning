bddd96489e3edf320a39e1ad540458c3a5d7d6fd
AdityaJoshiK <adityajoshi2107@gmail.com>
2023-11-16 13:15:28 +0530

    Model Fill and ActionName

--- ASP .Net MVC/TranningMVCApp/Controllers/EmployeeBLayerController.cs
+++ ASP .Net MVC/TranningMVCApp/Controllers/EmployeeBLayerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BusinessLayer;
+//using TranningMVCApp.Models;
 
 namespace TranningMVCApp.Controllers
 {
@@ -12,20 +13,39 @@ namespace TranningMVCApp.Controllers
         // GET: EmployeeBusinessLayer
         public ActionResult Index()
         {
-            EmployeeBuisnessLayer e = new BusinessLayer.EmployeeBuisnessLayer();
+            EmployeeBuisnessLayer e = new EmployeeBuisnessLayer();
 
             List<Employee> employees = e.Employees.ToList();
             return View(employees);
         }
 
         [HttpGet]
-        public ActionResult Create()
+        [ActionName("Create")]
+        public ActionResult Create_Get()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Create(FormCollection formCollection)
+        [ActionName("Create")]
+        public ActionResult Create_Post()
+        {
+            if (ModelState.IsValid)
+            {
+                EmployeeBuisnessLayer employeeblayer = new EmployeeBuisnessLayer();
+
+                Employee employee = new Employee();
+                UpdateModel<Employee>(employee);
+                employeeblayer.addEmployee(employee);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //Create Using Form Collection
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Createusingform(FormCollection formCollection)
         {
             Employee employee = new Employee();
             employee.Name= formCollection["Name"];
@@ -33,7 +53,7 @@ namespace TranningMVCApp.Controllers
             employee.Gender = formCollection["Gender"];
             employee.DateOfBirth = Convert.ToDateTime(formCollection["DateOfBirth"]);
 
-            EmployeeBuisnessLayer employeeblayer = new BusinessLayer.EmployeeBuisnessLayer();
+            EmployeeBuisnessLayer employeeblayer = new EmployeeBuisnessLayer();
             employeeblayer.addEmployee(employee);
 
             //foreach(string key in formCollection.Keys)
@@ -44,5 +64,31 @@ namespace TranningMVCApp.Controllers
             //}
             return RedirectToAction("Index");
         }
+
+        //Create Using Model
+        [HttpPost]
+        public ActionResult CreateUsingModel(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                EmployeeBuisnessLayer employeeblayer = new EmployeeBuisnessLayer();
+                employeeblayer.addEmployee(employee);   
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            // Code logic here
+            return HttpNotFound("Testing The resource was not found.");
+        }
+
+        public ActionResult JSON()
+        {
+            // Code logic here
+            return Json(new { Name = "Aditya" }, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
