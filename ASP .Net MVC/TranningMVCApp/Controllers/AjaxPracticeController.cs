a435c27bb14b637b39224f6198e0ebe14ca78e3c
AdityaJoshiK <adityajoshi2107@gmail.com>
2024-01-05 16:09:30 +0530

    AJAX and jquery autocomplere

--- ASP .Net MVC/TranningMVCApp/Controllers/AjaxPracticeController.cs
+++ ASP .Net MVC/TranningMVCApp/Controllers/AjaxPracticeController.cs
@@ -16,21 +16,53 @@ namespace TranningMVCApp.Controllers
             return View();
         }
 
+        public ActionResult List()
+        {
+            return View(newEmployeeEntities.NewEmployees.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult List(string searchTerm)
+        {
+            List<NewEmployee> employees = new List<NewEmployee>();
+
+            if (string.IsNullOrEmpty(searchTerm))
+            {
+                employees = newEmployeeEntities.NewEmployees.ToList();
+            }
+            else
+            {
+                employees = newEmployeeEntities.NewEmployees.Where(x=>x.FirstName.StartsWith(searchTerm)).ToList();
+            }
+            return View(employees);
+        }
+
+        public JsonResult GetEmployees(string term)
+        {
+            List<string> employees;
+
+            employees = newEmployeeEntities.NewEmployees.Where(x => x.FirstName.StartsWith(term)).Select(y => y.FirstName).ToList();
+
+            return Json(employees, JsonRequestBehavior.AllowGet);
+        }
+
         public PartialViewResult All()
         {
-            System.Threading.Thread.Sleep(4000);
+            System.Threading.Thread.Sleep(1000);
             List<NewEmployee> model = newEmployeeEntities.NewEmployees.ToList();
             return PartialView("_NewEmployees", model);
         }
 
         public PartialViewResult Top3()
         {
+            System.Threading.Thread.Sleep(1000);
             List<NewEmployee> model = newEmployeeEntities.NewEmployees.OrderByDescending(x => x.FirstName).Take(3).ToList();
             return PartialView("_NewEmployees", model);
         }
 
         public PartialViewResult Bottom3()
         {
+            System.Threading.Thread.Sleep(1000);
             List<NewEmployee> model = newEmployeeEntities.NewEmployees.OrderBy(x => x.FirstName).Take(3).ToList();
             return PartialView("_NewEmployees", model);
         }
