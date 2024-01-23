bad40939c105b6787c788eeb325157bb01be7511
AdityaJoshiK <adityajoshi2107@gmail.com>
2024-01-23 18:03:25 +0530

    Webapi project

---
+++ ASP .Net MVC/Web API MVC/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Web_API_MVC.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ASP .Net MVC/Web API MVC/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web_API_MVC.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employeenew> Get()
+        {
+            using (AdityaPracticeEntities db = new AdityaPracticeEntities())
+            {
+                return db.Employeenews.ToList();
+            }
+        }
+
+        public Employeenew Get(int id)
+        {
+            using (AdityaPracticeEntities db = new AdityaPracticeEntities())
+            {
+                return db.Employeenews.FirstOrDefault(x => x.Id == id);
+            }
+        }
+    }
+}
