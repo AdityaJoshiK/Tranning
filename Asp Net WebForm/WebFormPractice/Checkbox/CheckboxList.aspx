b303628701defbebf4c8fe8e57cf4bc17c6f729c
AdityaWebsmith <adityajoshi@websmithsolution.com>
2024-03-08 19:04:10 +0530

    Checkbox List In ASPX

---
+++ Asp Net WebForm/WebFormPractice/Checkbox/CheckboxList.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CheckboxList.aspx.cs" Inherits="WebFormPractice.Checkbox.CheckboxList" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:CheckBoxList ID="checkboxListEducation" runat="server"
+                RepeatDirection="Horizontal" RepeatColumns="2">
+                <asp:ListItem Text="Diploma" Value="1"></asp:ListItem>
+                <asp:ListItem Text="Graduate" Value="2"></asp:ListItem>
+                <asp:ListItem Text="Post Graduate" Value="3"></asp:ListItem>
+                <asp:ListItem Text="Doctrate" Value="4"></asp:ListItem>
+            </asp:CheckBoxList>
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="SelectAll" />
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Deselectall" />
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ Asp Net WebForm/WebFormPractice/Checkbox/CheckboxList.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormPractice.Checkbox
+{
+    public partial class CheckboxList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CheckBoxList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)    
+        {
+            if (checkboxListEducation.SelectedIndex != -1)
+            {
+                Response.Write(checkboxListEducation.SelectedItem.Text);
+            }
+            //foreach(ListItem li in checkboxListEducation.Items)
+            //{
+            //    if (li.Selected)
+            //    {
+            //        Response.Write("Text = " + li.Text + ", ");
+            //        // Retrieve the value of the selected list item
+            //        Response.Write("Value = " + li.Value + ", ");
+            //        // Retrieve the index of the selected list item
+            //        Response.Write("Index = " + checkboxListEducation.Items.IndexOf(li).ToString());
+            //        Response.Write("<br/>");
+            //    }
+            //}
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            foreach (ListItem li in checkboxListEducation.Items)
+            {
+                li.Selected = false;
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            foreach (ListItem li in checkboxListEducation.Items)
+            {
+                li.Selected = true;
+            }
+        }
+    }
+}
